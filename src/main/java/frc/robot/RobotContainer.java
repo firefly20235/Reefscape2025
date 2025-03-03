7132f397dc7778a14eb858b05dafd285cbe191f7
shlomitush <shlomit.talorot@gmail.com>
2025-03-03 16:22:03 +0200

    auto l4
    Co-Authored-By: Lavi Arzi <29277997+laviRZ@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -77,7 +77,7 @@ public class RobotContainer {
                 ()->-0.2,
                 ()->0,
                 ()->0
-        )).withTimeout(2).andThen(ElevatorCommands.moveToHeight(ElevatorState.L4).raceWith(OutputCommands.output(OutputState.L4).withTimeout(0.5))).withTimeout(3).
+        )).withTimeout(2).raceWith(ElevatorCommands.moveToHeight(ElevatorState.L4).andThen(OutputCommands.output(OutputState.L4).withTimeout(2))).
         andThen((SwerveCommands.getFieldRelativeClosedLoopSupplierDriveCommand(
             ()->-0,
             ()->0,
